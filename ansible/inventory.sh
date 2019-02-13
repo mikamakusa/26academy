bfb113905646071a59da56bd779bf27971be932e
Mickael DANGLETERRE <mickael_dangleterre@carrefour.com>
2019-02-14 00:19:42 +0100

    update gitlab-ci + inventory script

--- ansible/inventory.sh
+++ ansible/inventory.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-python ../terraform/digitalocean.py --list > /opt/inventory.json
-
-echo "[docker]" > ansible/inventory.ini
-
-END=5
-for (( i = 2; i < $END; i++ )); do
-	name=$(cat /opt/inventory.json | jq -r ".[$i].name")
-	ip=$(cat /opt/inventory.json | jq -r ".[$i].networks.v4[].ip_address")
-
-	echo $name ansible_ssh_host=$ip ansible_ssh_private_key_file=ssh_keys/ssh_keys/26apro2 ansible_user=root >> ansible/inventory.ini
-done
+echo [docker] > ansible/inventory.ini
+python digitalocean.py --droplets | jq -r '.droplets[] | .name +" ansible_ssh_host="+ .networks.v4[].ip_address' >> ansible/inventory.ini
+sed -i '/blog1/d' ansible/inventory.ini && sed -i '/bastion/d' ansible/inventory.ini
