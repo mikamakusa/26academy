032122f50ef7fc59c7cbccf646490f684ff667ba
Mickael DANGLETERRE <mickael_dangleterre@carrefour.com>
2019-02-14 01:16:10 +0100

    update

--- ansible/inventory.sh
+++ ansible/inventory.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo [docker] > ansible/inventory.ini
-python digitalocean.py --droplets | jq -r '.droplets[] | .name +" ansible_ssh_host="+ .networks.v4[].ip_address' >> ansible/inventory.ini
+python ansible/digitalocean.py --droplets | jq -r '.droplets[] | .name +" ansible_ssh_host="+ .networks.v4[].ip_address' >> ansible/inventory.ini
 sed -i '/blog1/d' ansible/inventory.ini && sed -i '/bastion/d' ansible/inventory.ini
